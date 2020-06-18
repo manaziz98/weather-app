85591aed736a7fa04c09bf8cf472302abc85b11a
aziz manai <zizoumanai98@gmail.com>
2020-06-18 01:03:30 +0100

    api key secured

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 /* fetching weather data from api */
-const API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+const API_KEY = "xxxxxxxxxxxxxxxxxxxxxx";
 const fetchWheatherData = (cityName) => {
   const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=imperial`;
 
